c239f95d2d7ebb3608a3885cd08277376fd4afda
Filip Zoric <fzoric8@gmail.com>
2023-07-19 19:37:46 +0200

    Added succesful inference script

--- scripts/mmros_wrapper.py
+++ scripts/mmros_wrapper.py
@@ -18,14 +18,16 @@ class MMRosWrapper:
         self.model_initialized = False
         rospy.init_node('image_subscriber_node', anonymous=True)
         self.rate = rospy.Rate(10)  # 10 Hz, adjust as needed
-        self.compr_img_sub = rospy.Subscriber("camera/color/image_raw/compressed", CompressedImage, self.image_callback)
+        self.compr_img_sub = rospy.Subscriber("camera/color/image_raw/compressed", CompressedImage, self.image_callback, queue_size=1)
         self.model = self.load_model(deploy_cfg_path, model_cfg_path, backend_model_name)
 
     def image_callback(self, data):
         try:
             np_arr = np.fromstring(data.data, np.uint8)
             img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-            self.img = img
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            self.img = img_rgb
+            self.np_arr = np_arr
             self.img_received = True
         except Exception as e:
             rospy.logerr("Error decoding compressed image: %s", str(e))
@@ -41,27 +43,39 @@ class MMRosWrapper:
         device = "cuda:0"
 
         # Build task and backend model
-        task_processor = build_task_processor(self.model_cfg, self.deploy_cfg, device)
-        model = task_processor.build_backend_model([backend_model_name])
+        self.task_processor = build_task_processor(self.model_cfg, self.deploy_cfg, device)
+        model = self.task_processor.build_backend_model([backend_model_name])
         self.model_initialized = True
         rospy.loginfo("Model succesfully initialized!")
 
         return model
 
     def run(self):
+        i = 0
         while not rospy.is_shutdown():
+            viz_dbg_img_pth = "/home/gideon/catkin_ws/src/mmros_wrapper/scripts"
             if self.model_initialized and self.img_received:
+                i += 1
                 img = self.img.copy()
 
                 # Process input image
                 input_shape = get_input_shape(self.deploy_cfg)
-                model_inputs, _ = task_processor.create_input(img, input_shape)
+                model_inputs, _ = self.task_processor.create_input(img, input_shape)
 
                 # Capture the start time
                 start_time = rospy.Time.now()
 
                 with torch.no_grad():
                     result = self.model.test_step(model_inputs)
+                
+                debug_imgs = True
+                if debug_imgs:
+                    self.task_processor.visualize(
+                         image = img,
+                         model = self.model,
+                         result = result[0],
+                         window_name='visualize',
+                         output_file="{}/debug_imgs/out_{}.png".format(viz_dbg_img_pth, i))
 
                 # Capture the end time and calculate the duration
                 end_time = rospy.Time.now()
