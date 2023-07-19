fedca69ff5a43d92b38aa58da6dae76acdc21943
Filip Zoric <fzoric8@gmail.com>
2023-07-19 17:17:52 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# MMROS Wrapper
+
+## Overview
+MMROS Wrapper is a ROS package that provides a simple node, `MMRosWrapper`, which subscribes to an image topic, performs instance segmentation using the `mmdetection` framework with the help of `mmdeploy`, and collects litter objects from the images. It uses `torch` for neural network computations.
+
+## Dependencies
+This package relies on the following external libraries and frameworks:
+
+- [torch](https://pytorch.org/): PyTorch is an open-source machine learning library used for tensor computations and deep learning.
+- [mmdeploy](https://github.com/open-mmlab/mmdeploy): MMDEPLOY is an efficient deep learning deployment toolbox built on top of PyTorch.
+- [mmdetection](https://github.com/open-mmlab/mmdetection): MMDetection is an open-source object detection toolbox based on PyTorch.
+
+Make sure to install these dependencies before running the ROS package.
+
+## Launching the Node
+To launch the `MMRosWrapper` node, use the provided launch file:
+
+```
+roslaunch mmros_wrapper launch_mmr_ros_wrapper.launch deploy_cfg_path:=path/to/deploy_cfg.yaml model_cfg_path:=path/to/model_cfg.yaml backend_model_name:=your_backend_model_name
+```
+
+Replace path/to/deploy_cfg.yaml and path/to/model_cfg.yaml with the appropriate file paths for your configuration. Also, provide the your_backend_model_name that corresponds to the backend model you want to use for instance segmentation.
+
+## Instance Segmentation for Litter Collection with a Drone
+
+Instance segmentation is a computer vision task that involves detecting and segmenting each individual object instance in an image. In this ROS package, the instance segmentation technique is utilized to detect and collect litter objects from images captured by a drone.
+
+When the MMRosWrapper node is running, it subscribes to the specified image topic, processes the received images using the mmdetection framework, and identifies individual litter objects in the scene. This information can be further used to plan and execute litter collection actions with the drone.
+
+Note: The ROS package and the node are provided as a basic example, and for real-world applications, additional components such as object detection and control logic for the drone would be needed to create a fully functional litter collection system.
+
+Feel free to modify and extend this ROS package to suit your specific litter collection use case with a drone.
+
+## TODO
+
+- [ ] Implement control logic for drone litter collection.
+- [ ] Integrate object detection to identify additional types of litter.
+- [ ] Optimize the instance segmentation process for real-time operation.
+- [ ] Add ROS topics for publishing the detected litter objects.
---
+++ scripts/mmros_wrapper.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import CompressedImage
+import cv2
+import numpy as np
+
+from mmdeploy.apis.utils import build_task_processor
+from mmdeploy.utils import get_input_shape, load_config
+from PIL import Image
+import torch
+
+class MMRosWrapper:
+    def __init__(self, deploy_cfg_path, model_cfg_path, backend_model_name):
+        self.img = None
+        self.img_received = False
+        self.model_initialized = False
+        rospy.init_node('image_subscriber_node', anonymous=True)
+        self.rate = rospy.Rate(10)  # 10 Hz, adjust as needed
+        self.compr_img_sub = rospy.Subscriber("camera/color/image_raw/compressed", CompressedImage, self.image_callback)
+        self.model = self.load_model(deploy_cfg_path, model_cfg_path, backend_model_name)
+
+    def image_callback(self, data):
+        try:
+            np_arr = np.fromstring(data.data, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            self.img = img
+            self.img_received = True
+        except Exception as e:
+            rospy.logerr("Error decoding compressed image: %s", str(e))
+
+    def load_model(self, deploy_cfg_path, model_cfg_path, backend_model_name):
+        # Read deploy_cfg and model_cfg
+        deploy_cfg, model_cfg = load_config(deploy_cfg_path, model_cfg_path)
+
+        # Replace the following line with the appropriate device selection if needed
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Build task and backend model
+        task_processor = build_task_processor(model_cfg, deploy_cfg, device)
+        model = task_processor.build_backend_model(backend_model_name)
+        self.model_initialized = True
+	rospy.loginfo("Model succesfully initialized!")
+
+        return model
+
+    def run(self):
+        while not rospy.is_shutdown():
+            if self.model_initialized and self.img_received:
+                img = self.img.copy()
+
+                # Process input image
+                input_shape = get_input_shape(deploy_cfg)
+                model_inputs, _ = task_processor.create_input(img, input_shape)
+
+                # Capture the start time
+                start_time = rospy.Time.now()
+
+                with torch.no_grad():
+                    result = model.test_step(model_inputs)
+
+                # Capture the end time and calculate the duration
+                end_time = rospy.Time.now()
+                duration = (end_time - start_time).to_sec()
+
+                print("Duration of model.test_step(model_inputs):", duration)
+            else:
+                if not self.model_initialized:
+                    rospy.logwarn("Model not initialized yet.")
+                if not self.img_received:
+                    rospy.logwarn("Image not received yet.")
+
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) < 4:
+            print("Usage: rosrun your_package_name MMRosWrapper.py deploy_cfg_path model_cfg_path backend_model_name")
+        else:
+            deploy_cfg_path = sys.argv[1]
+            model_cfg_path = sys.argv[2]
+            backend_model_name = sys.argv[3]
+            mmWrap = MMRosWrapper(deploy_cfg_path, model_cfg_path, backend_model_name)
+            mmWrap.run()
+    except rospy.ROSInterruptException:
+        pass
