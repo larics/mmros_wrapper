059abfc067f23b5e90cc3cfb847b8716c4923c44
Filip Zoric <fzoric8@gmail.com>
2024-02-01 15:54:05 +0100

    Added crack localizer

--- scripts/crack_localizer.py
+++ scripts/crack_localizer.py
@@ -5,22 +5,25 @@ import sys
 import io
 import copy
 import numpy as np
+import sensor_msgs.point_cloud2 as pc2
 
 from sensor_msgs.msg import Image, CameraInfo, PointCloud2
 from mmros_utils.msg import InstSegArray, InstSeg
+from visualization.msg import MarkerArray
+
 from utils import ros_image_to_numpy
 from matplotlib import pyplot as plt
 
 def find_indexes(array, x):
     indexes = np.where(array == x)
-    return list(zip(indexes[0], indexes[1]))
+    return list(zip(indexes[1], indexes[0]))
 
 class CrackLocalizer(): 
 
     def __init__(self):
         rospy.loginfo("Initializing node!")
         rospy.init_node('shm_planner', anonymous=True, log_level=rospy.DEBUG)
-        self.rate = rospy.Rate(20)
+        self.rate = rospy.Rate(1)
         self.inst_seg_reciv = False; self.pcl_reciv = False;
         # Initialize subscribers and publishers in the end!
         self._init_subscribers()
@@ -30,7 +33,7 @@ class CrackLocalizer():
         self.pcl_sub = rospy.Subscriber("/camera/depth_registered/points", PointCloud2, self.pcl_cb, queue_size=1)
 
     def _init_publishers(self): 
-        pass
+        self.viz_pub = rospy.Publisher("/viz/markers", MarkerArray)
 
 
     def get_depths(self, pcl, indices, axis="z"):
@@ -48,20 +51,30 @@ class CrackLocalizer():
     def pcl_cb(self, msg): 
 
         self.pcl_reciv = True
-        self.pcl_msg = msg
+        self.pcl = msg
+
+    def localize_crack(mask_msg, sample=1):
+        # Get mask
+        mask_ = self.inst_seg.instances[0].mask
+        np_mask = ros_image_to_numpy(mask_)
+        indices = find_indexes(np_mask, 255)
+        indices_ = indices[::sample]
+        points = list(pc2.read_points(self.pcl, skip_nans=True, uvs=indices, field_names = ("x", "y", "z")))
+        return points
 
     def run(self): 
         self.cnt = 0
         while not rospy.is_shutdown(): 
             if self.inst_seg_reciv and self.pcl_reciv: 
-                # Get mask
-                mask_ = self.inst_seg.instances[0].mask
+                mask = self.inst_seg.instances[0].mask
+                pts = self.localize_crack(mask)
+                #print(mask_)
                 # Get indices for that mask
-                indices = find_indexes(mask_, 255)
-                rospy.logdebug(f"Indices are: {indices}")
+                #indices = find_indexes(mask_, 255)
+                #rospy.logdebug(f"Indices are: {indices}")
                 # Get 3D points for that indices
-                points = get_depths(self.pcl, indices)
-                rospy.logdebug(f"Points are: {points}")
+                #points = get_depths(self.pcl, indices)
+                #rospy.logdebug(f"Points are: {points}")
             else: 
                 rospy.logwarn(f"Img reciv: {self.inst_seg_reciv}; PCL reciv: {self.pcl_reciv}")
             self.rate.sleep()
